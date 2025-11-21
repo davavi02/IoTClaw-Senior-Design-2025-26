73b1ee5b50ca1812c946cc48e8ceb38ca180e6dd
Phillip Cook <philcsound@gmail.com>
2025-11-21 00:03:59 -0600

    Made a basic userdata store, there's a temporary token adjuster here as well.

---
+++ frontend/stores/UserDataStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+
+interface UserData {
+    username: string;
+    numTokens: number;
+
+    setTokens: (num: number) => void;
+
+}
+
+const useUserDataStore = create<UserData>()((set, get) => ({
+  username: "",
+  numTokens: 0,
+
+  setTokens: (num: number) => {
+    const curTokens = get().numTokens;
+    set({numTokens: curTokens + num});
+  },
+}));
+
+
+export default useUserDataStore;
