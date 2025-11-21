f5698f722769b3e765843633ce5f98556bf4a48c
David <74672434+davavi02@users.noreply.github.com>
2025-11-21 17:41:58 +0000

    Added serial connection to MCU

--- hardware/PC Client/keyboard_test.py
+++ hardware/PC Client/keyboard_test.py
@@ -3,9 +3,11 @@ Program to test keyboard input for controlls.
 Press/hold [SPACE], [UP], [DOWN], [LEFT], [RIGHT] to test.
 Press/hold 'Q' or 'q' to quit.
 """
-
+import serial
 import keyboard
 import time
+
+gantry = serial.Serial('COM4', 115200)
 
 def main():
     print("Press arrow keys or SPACE. Type Q to quit.\n")
@@ -22,14 +24,22 @@ def main():
         # Check each key
         if keyboard.is_pressed('space'):
             current = "space"
+            serout = 1
         elif keyboard.is_pressed('up'):
             current = "up"
+            serout = 5
         elif keyboard.is_pressed('down'):
             current = "down"
+            serout = 4
         elif keyboard.is_pressed('left'):
             current = "left"
+            serout = 2
         elif keyboard.is_pressed('right'):
             current = "right"
+            serout = 3
+        elif keyboard.is_pressed('c'):
+            current = "coin"
+            serout = 6
         else:
             current = None
 
@@ -41,15 +51,18 @@ def main():
 
         if current is None:
             print(0)
+            gantry.write(b'0\n')
             last_pressed = None
         else:
             # Newly pressed
             if current != last_pressed:
                 print(current)
+                gantry.write(bytes(str(serout) + '\n', encoding="utf-8"))
                 last_pressed = current
             else:
                 # Held down, keep printing
                 print(current)
+                gantry.write(bytes(str(serout) + '\n', encoding="utf-8"))
 
         # Output rate
         time.sleep(0.1)
---
+++ hardware/PC Client/readme.md
@@ -0,0 +1,2 @@
+# Python Code for Computer
+Code to control the MCU from a computer with keyboard inputs
