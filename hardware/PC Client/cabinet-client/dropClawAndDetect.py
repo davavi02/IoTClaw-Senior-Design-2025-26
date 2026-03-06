c4cdbf4857342e4a52cf4e17437720d5e60c9f22
davavi02 <davavi02@gmail.com>
2026-03-06 10:43:21 -0600

    Serial websocket initial draft

--- hardware/PC Client/cabinet-client/dropClawAndDetect.py
+++ hardware/PC Client/cabinet-client/dropClawAndDetect.py
@@ -1,5 +1,7 @@
 # The drop move is seperate from the moveClaw as its an "endgame" move
 # After the claw is dropped, a check is done to see if a prize was won or not
+from moveClaw import moveClaw
 
 async def dropClawAndDetect():
     print("Dropping and Detecting")
+    moveClaw(5)
--- hardware/PC Client/cabinet-client/moveClaw.py
+++ hardware/PC Client/cabinet-client/moveClaw.py
@@ -2,5 +2,26 @@
 # Talks to the MCU through Serial
 import serial
 
+claw = serial.Serial("COM4", 115200)
+
 async def moveClaw(command):
     print("Moving claw: ", command)
+    if command == 0:
+        # Stop
+        claw.write(b"0\n")
+    elif command == 1:
+        # Up
+        claw.write(b"5\n")
+    elif command == 2:
+        # Down
+        claw.write(b"4\n")
+    elif command == 3:
+        # Left
+        claw.write(b"2\n")
+    elif command == 4:
+        # Right
+        claw.write(b"3\n")
+    elif command == 5:
+        # Drop claw and stop
+        claw.write(b"1\n")
+        claw.write(b"0\n")
